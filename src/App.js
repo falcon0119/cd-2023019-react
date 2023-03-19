f3a78627f7c6cde33df4c3c84d24d7e83b3b086a
sockeye119 <58209598+sockeye119@users.noreply.github.com>
2023-03-20 00:00:17 +0900

    表示切り替え

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { ChildArea } from "./component/childArea";
 
 export default function App() {
-  const [count, setcount] = useState(1);
-  const onClickchange = () => {
-    setcount(count + 1);
-  };
+  const [text, settext] = useState("");
+  const [open, setopen] = useState(false);
+  const onChangetext = (e) => settext(e.target.value);
+  const onClickchange = () => setopen(!open);
+
   return (
-    <dev class="counter">
-      <h1>カウンターアプリ</h1>
-      <p>{count}</p>
-      <button onClick={onClickchange}>カウント</button>
-    </dev>
+    <>
+      <dev class="counter">
+        <h1>表示切り替え</h1>
+        <input value={text} onChange={onChangetext} />
+        <br />
+        <button onClick={onClickchange}>表示</button>
+        <ChildArea open={open} />
+      </dev>
+    </>
   );
 }
