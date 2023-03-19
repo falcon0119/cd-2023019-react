e718cc21f3cbbdc2ba88063dd97932b1d0392e51
sockeye119 <58209598+sockeye119@users.noreply.github.com>
2023-03-19 23:28:36 +0900

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [count, setcount] = useState(1);
+  const onClickchange = () => {
+    setcount(count + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <dev class="counter">
+      <h1>カウンターアプリ</h1>
+      <p>{count}</p>
+      <button onClick={onClickchange}>カウント</button>
+    </dev>
   );
 }
-
-export default App;
